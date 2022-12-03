7873c2cc5b2b48a85fa8b66e28015f516c52c5fe
paul <paul@stratofex.com>
2022-12-03 15:12:10 -0800

    more stupid bugs

--- tweet2csv.py
+++ tweet2csv.py
@@ -5,33 +5,35 @@ from pprint import pprint
 hashtag = re.compile(r"(#+[a-zA-Z0-9(_)]{1,})")
 username = re.compile(r"(@+[a-zA-Z0-9(_)]{1,})")
 
+
 class Tweet:
-    
+
     def __init__(self):
         self.time = ""
         self.user = ""
         self.tweet = ""
         self.hashtags = ""
         self.mentions = ""
-    
+
     def output(self):
         self.hashtags = hashtag.findall(self.tweet)
         self.mentions = username.findall(self.tweet)
-        return(f"\"{self.time}\",\"{self.user}\",\"{self.tweet}\",\"{self.mentions}\",\"{self.hashtags}\"")
-    
+        return (
+            f"\"{self.time}\",\"{self.user.encode()}\",\"{self.tweet.encode()}\",\"{self.mentions.encode()}\",\"{self.hashtags.encode()}\"")
+
     def strip(self, s):
-        return(s.split("\t")[1])
-        
+        return (s.split("\t")[1].encode())
+
     def itime(self, s):
         self.time = self.strip(s.strip())
-        
+
     def iuser(self, s):
         self.user = self.strip(s.strip().replace("http://twitter.com/", "@"))
-        
+
     def itweet(self, s):
         self.tweet = self.strip(s.strip())
-        
-    
+
+
 t = Tweet()
 print("starting")
 for l in sys.stdin:
@@ -40,16 +42,13 @@ for l in sys.stdin:
         print(t.output())
         t = Tweet()
 
-        
     if l.startswith("T\t"):
         t.itime(l)
-        
+
     if l.startswith("U\t"):
         t.iuser(l)
-        
+
     if l.startswith("W\t"):
         t.itweet(l)
-        
+
     # print(f'Input : {l}')
-    
-    

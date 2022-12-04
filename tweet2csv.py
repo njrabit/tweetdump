f9e0914d8293f5d540061f80f8926d1843fd8f9c
paul <paul@stratofex.com>
2022-12-04 09:30:23 -0800

    doh, python rustyness

--- tweet2csv.py
+++ tweet2csv.py
@@ -43,12 +43,12 @@ class Tweet:
         self.tweet = self.trim(s)
 
     def ihashtag(self):
-        if(type(s)==string):
+        if(type(s)=="string"):
             s = dehash.findall(self.tweet)
             self.hashtag = s.sort() 
 
     def imention(self):
-        if(type(s)==string):
+        if(type(s)=="string"):
             s = demention.findall(self.tweet)
             self.mention = s.sort()
         
